89044114a7c7f3956ba39553b0bd500942dc21e0
BlueBirb <dav_rec@icloud.com>
2023-05-08 23:43:20 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+from get_token import get_tok
+
+intents = discord.Intents.all()
+
+client = commands.Bot(command_prefix='.', intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('Bot start')
+
+client.run(get_tok())
